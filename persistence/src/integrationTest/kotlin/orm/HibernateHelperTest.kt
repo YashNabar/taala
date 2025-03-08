f668d6539654fc32a7427ce7dfc499a807c2cf2d
Yash Nabar <nabar.yash@gmail.com>
2025-03-08 21:30:17 +0000

    Use Postgres and Hikari for persistence

---
+++ persistence/src/integrationTest/kotlin/orm/HibernateHelperTest.kt
@@ -0,0 +1,48 @@
+package orm
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.assertj.core.api.Assertions.assertThat
+import org.hibernate.SessionFactory
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
+import taala.persistence.orm.HibernateHelper
+
+class HibernateHelperTest {
+
+    @Test
+    fun `given postgres datasource, when buildSessionFactory, then returns session factory`() {
+        sessionFactory.openSession().use { session ->
+            assertThat(session.isOpen).isTrue()
+        }
+    }
+
+    private companion object {
+        lateinit var sessionFactory: SessionFactory
+        val database = PostgreSQLContainer(DockerImageName.parse("postgres:17"))
+
+        @JvmStatic
+        @BeforeAll
+        fun setUp() {
+            database.start()
+            sessionFactory = HibernateHelper.buildSessionFactory(
+                HikariDataSource(
+                    HikariConfig().apply {
+                        jdbcUrl = database.jdbcUrl
+                        username = database.username
+                        password = database.password
+                    }
+                )
+            )
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun tearDown() {
+            database.stop()
+        }
+    }
+}
